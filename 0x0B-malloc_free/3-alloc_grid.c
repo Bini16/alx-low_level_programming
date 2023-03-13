48de63c5920a0150c31740d34b134b7499115d37
Biniyam Abraham <abrahambiniyam6@gmail.com>
2023-03-13 12:12:19 -0700

    If you even dream of beating me youd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,10 +6,10 @@
  * integers with each element initalized to 0.
  * @width: The width of the 2-dimensional array.
  * @height: The height of the 2-dimensional array.
- *
  * Return: If width <= 0, height <= 0, or the function fails - NULL.
  * Otherwise - a pointer to the 2-dimensional array of integers.
  */
+
 int **alloc_grid(int width, int height)
 {
 	int **twoD;
@@ -22,19 +22,19 @@ int **alloc_grid(int width, int height)
 	return (NULL);
 	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		twoD[hgt_index] = malloc(sizeof(int) * width);
+	twoD[hgt_index] = malloc(sizeof(int) * width);
 	if (twoD[hgt_index] == NULL)
 	{
 	for (; hgt_index >= 0; hgt_index--)
-		free(twoD[hgt_index]);
-		free(twoD);
+	free(twoD[hgt_index]);
+	free(twoD);
 	return (NULL);
 	}
 	}
 	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
 	for (wid_index = 0; wid_index < width; wid_index++)
-		twoD[hgt_index][wid_index] = 0;
+	twoD[hgt_index][wid_index] = 0;
 	}
 	return (twoD);
 }
